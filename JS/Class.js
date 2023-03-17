ebb8ea6ed8419c092c45569ded418c277dce3caa
Lacan <lacan.xrl80@gmail.com>
2023-03-17 09:37:46 -0400

    Concertando comentários e adicionando pasta js

---
+++ JS/Class.js
@@ -0,0 +1,15 @@
+//Criando uma classe
+//Classe baseada em Pessoa
+class Person{
+    constructor(name, age, id, cpf){ //Criação da classe construtora
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.cpf;
+    }
+}
+
+
+//Instanciado a classe Pessoa
+
+let pessoa_1 = new Person("Rodrigo Lacan","17","001","123.456.789-00")
