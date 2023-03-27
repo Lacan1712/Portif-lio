3bd26ce280accc968a5c0afb51a3850d3602fb20
Lacan <lacan.xrl80@gmail.com>
2023-03-27 08:50:43 -0400

    Comportamento de rotação

--- JS/LUTA JS/assets/js/Script.js
+++ JS/LUTA JS/assets/js/Script.js
@@ -1,9 +1,11 @@
 class Personagem{
     _life = 100;
     element;
+    mouseX;
+    mouseY;
     constructor(x, y, image){
-        this.positionX = x;
-        this.positionY = y;
+        this._positionX = x;
+        this._positionY = y;
         this.element = image;
     }
 
@@ -17,11 +19,46 @@ class Personagem{
         this._life = newLife < 0 ? 0 : newLife;
     }
 
+    //get positionX e get PositionY
+    getPositionX(){
+        this._positionX
+    }
+    getPositionY(){
+        this._positionY
+    }
 
+    //set positionX e positionY
+    setPositionX(newPX){
+        this._positionX = newPX;
+    }
+    setPositionY(newPY){
+        this._positionY = newPY;
+    }
+
+
+    //Método que define posição inicial do elemento
     position(){
-        this.element.style.left = 50 + "px";
-        this.element.style.left = 50 + "px";
+        this.element.style.left = this._positionX + "px";
+        this.element.style.top = this._positionY + "px";
 
+    }
+
+    moveRotate(X, Y){
+            //Capturar parametros da localização do mouse
+            this.mouseX = X,
+            this.mouseY = Y;
+            //Obetendo ponto do centro do elemento
+            var naveX = this.element.offsetLeft + (this.element.offsetWidth / 2);
+            var naveY = this.element.offsetTop + (this.element.offsetHeight / 2);
+    
+            //Calcular ângulo de rotação
+            var radianos = Math.atan2 (this.mouseY - naveY, this.mouseX - naveX);
+            var angulo = radianos * (180 / Math.PI);
+    
+            //Definindo propriedade transforme ao elemento (nave)
+            this.element.style.transform = 'rotate(' + angulo + 'deg)';
+    
+    
     }
     
     
